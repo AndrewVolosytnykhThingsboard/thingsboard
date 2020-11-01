41308b79f6c839fc896103b8893681e799841686
Valerii Sosliuk <valerii@thingsboard.io>
2020-11-01 22:11:04 +0200

    Added missing import

--- rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/mail/TbMsgToEmailNode.java
+++ rule-engine/rule-engine-components/src/main/java/org/thingsboard/rule/engine/mail/TbMsgToEmailNode.java
@@ -31,6 +31,7 @@
 package org.thingsboard.rule.engine.mail;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
